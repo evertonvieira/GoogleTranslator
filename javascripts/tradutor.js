efb6ecb72f00e59c088554fe99647943c3f897f6
Everton Vieira <everton@sevensolucoes.com.br>
2015-03-26 13:32:56 -0300

    Adicionando demo do plugin

---
+++ javascripts/tradutor.js
@@ -0,0 +1,46 @@
+function ChangeLang(a) {
+	var b, elemento = "";
+	if (document.createEvent) {
+		var c = document.createEvent("HTMLEvents");
+		c.initEvent("click", true, false)
+	}
+	if (a == 'pt') {
+		elemento = $(".goog-te-banner-frame:eq(0)").contents().find("button[id*='restore']")
+	} else {
+		switch (a) {
+		case 'de':
+			b = "alem";
+			break;
+		case 'es':
+			b = "Spanish";
+			break;
+		case 'fr':
+			b = "fran";
+			break;
+		case 'en':
+			b = "English";
+			break;
+		case 'it':
+			b = "italiano";
+			break
+		}
+		elemento = $(".goog-te-menu-frame:eq(0)").contents().find("span:contains('" + b + "')");
+		//alert(b);
+	}
+	if (elemento.length > 0) {
+		if (document.createEvent) {
+			elemento[0].dispatchEvent(c)
+		} else {
+			elemento[0].click()
+		}
+	}
+}
+function googleTranslateElementInit() {
+	new google.translate.TranslateElement({
+		pageLanguage: 'pt',
+		autoDisplay: false,
+		includedLanguages: 'de,es,fr,en,it',
+		layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+	},
+	'google_translate_element');
+}
